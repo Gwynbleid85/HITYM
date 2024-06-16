5e6b2c57875f46696c63fcc626e63015e8d07657
Jiri Mladek <xmladek@fi.muni.cz>
2024-06-16 17:30:55 +0200

    feat: delete unused UI folder

--- ui/src/button.tsx
+++
@@ -1,9 +0,0 @@
-import type { PropsWithChildren } from "react";
-
-interface ButtonProps extends PropsWithChildren {
-  className?: string;
-}
-
-export const Button = ({ children, className }: ButtonProps) => {
-  return <button className={className}>{children}</button>;
-};
