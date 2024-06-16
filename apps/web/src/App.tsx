fae7db9c0bddb45ee27bbc8a4cd898ecebc19c20
Jiri Mladek <xmladek@fi.muni.cz>
2024-06-16 22:36:58 +0200

    feat: Init shadcn/tailwind

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,53 +1,10 @@
-import themeOptions from "../mui.theme.json";
+import { Button } from "@/components/ui/button";
 
-import { createTheme, ThemeProvider, type PaletteColor, type ThemeOptions } from "@mui/material";
-import Home from "./pages/Home";
-
-declare module "@mui/material/styles" {
-  interface PaletteOptions {
-    custom: PaletteOptions["primary"];
-    dark: PaletteOptions["primary"];
-    light: PaletteOptions["primary"];
-  }
-}
-
-declare module "@mui/material/styles" {
-  interface Palette {
-    custom: PaletteColor;
-    dark: PaletteColor;
-    light: PaletteColor;
-  }
-}
-
-interface ColorOverrides {
-  custom: true;
-  light: true;
-  dark: true;
-}
-
-declare module "@mui/material/Button" {
-  interface ButtonPropsColorOverrides extends ColorOverrides {}
-}
-
-declare module "@mui/material/IconButton" {
-  interface IconButtonPropsColorOverrides extends ColorOverrides {}
-}
-
-declare module "@mui/material/ButtonGroup" {
-  interface ButtonGroupPropsColorOverrides extends ColorOverrides {}
-}
-
-declare module "@mui/material/AppBar" {
-  interface AppBarPropsColorOverrides extends ColorOverrides {}
-}
-
-export function App() {
-  const theme = createTheme(themeOptions as ThemeOptions);
-
+function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Home />
-    </ThemeProvider>
+    <div className="m-5">
+      <Button>Click me</Button>
+    </div>
   );
 }
 
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,12 +1,8 @@
-import ReactDOM from "react-dom/client";
 import React from "react";
-import App from "./App";
-import CssBaseline from "@mui/material/CssBaseline";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./globals.css";
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -14,7 +10,6 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <CssBaseline />
       <App />
     </QueryClientProvider>
   </React.StrictMode>
