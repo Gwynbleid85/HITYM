c547238d2bb837df5ea5396d88826474ecc88c0b
Jiri Mladek <xmladek@fi.muni.cz>
2024-06-17 19:45:11 +0200

    feat: Clickable map behind divs

--- apps/web/src/layouts/MainLayout.tsx
+++ apps/web/src/layouts/MainLayout.tsx
@@ -3,10 +3,14 @@ import Map from "@/components/Map";
 
 export function MainLayout() {
   return (
+
     // TODO navbar
+    //TODO center, compute
     <>
       <Map />
-      <Outlet />
+      <div id="hovno" className="absolute inset-0 w-fit h-fit flex items-center justify-center">
+        <Outlet />
+      </div>
     </>
   );
 }
