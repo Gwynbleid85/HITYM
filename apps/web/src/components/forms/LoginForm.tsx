8ec93058e79b97581649e7054679af80dcfd787c
Jiri Mladek <xmladek@fi.muni.cz>
2024-06-18 19:00:57 +0200

    feat: Validating login and Sign up forms

--- apps/web/src/components/forms/LoginForm.tsx
+++ apps/web/src/components/forms/LoginForm.tsx
@@ -1,37 +1,82 @@
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { ButtonLoading } from "../ui/button-loading";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+
+import { Link } from "react-router-dom";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import type { UserLoginRequest } from "@/types/Api";
+import { userLoginDataSchema } from "@/validationSchemas/user.validationSchemas";
 
 export function LoginForm() {
+  const form = useForm<UserLoginRequest>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(userLoginDataSchema),
+  });
+
+  function onSubmit(values: UserLoginRequest) {
+    console.log(values);
+  }
+
   return (
-    <Card className="w-full max-w-sm bg-white bg-opacity-95 rounded-xl m-5 ">
-      <CardHeader className="items-center justify-center">
-        <CardTitle className="text-2xl">Login</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account.
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="hitym@example.com" required />
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" required />
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full">Sign in</Button>
-      </CardFooter>
+    <Card className="w-full max-w-sm bg-white bg-opacity-95 rounded-xl m-5 shadow-xl">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <CardHeader className="items-center justify-center">
+            <CardTitle className="text-3xl">Login</CardTitle>
+            <CardDescription>Enter email and password to login to your account.</CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-3">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="font-medium">Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="hitym@example.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="font-medium">Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+          <CardFooter className="flex-col">
+            {!form.formState.isSubmitting ? (
+              <Button type="submit" className="w-5/6">
+                Sign in
+              </Button>
+            ) : (
+              <ButtonLoading />
+            )}
+
+            <div className="mt-4 text-center text-sm">
+              Don't have an account?{" "}
+              <Link to="/signup" className="underline">
+                Sign up
+              </Link>
+            </div>
+          </CardFooter>
+        </form>
+      </Form>
     </Card>
-  )
+  );
 }
--- apps/web/src/router/routes.tsx
+++ apps/web/src/router/routes.tsx
@@ -1,27 +1,31 @@
 import { Navigate, type RouteObject } from "react-router-dom";
 import Login from "@/pages/Login";
+import SignUp from "@/pages/SignUp";
 import MainLayout from "@/layouts/MainLayout";
 
 // TODO Not found page
 
-
 const mainLayoutRoutes: RouteObject[] = [
-    {
-      path: "login",
-      Component: Login,
-    },
-  ];
-  
-  const routes: RouteObject[] = [
-    {
-      path: "/",
-      Component: MainLayout,
-      children: mainLayoutRoutes,
-    },
-    // {
-    //   path: "*",
-    //   Component: ErrorsNotFound,
-    // },
-  ];
-  
-  export default routes;
+  {
+    path: "login",
+    Component: Login,
+  },
+  {
+    path: "signup",
+    Component: SignUp,
+  },
+];
+
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    Component: MainLayout,
+    children: mainLayoutRoutes,
+  },
+  // {
+  //   path: "*",
+  //   Component: ErrorsNotFound,
+  // },
+];
+
+export default routes;
