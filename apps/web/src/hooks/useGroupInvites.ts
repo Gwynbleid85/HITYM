a345f2c4220efc0594eda59a015f7132ceffd383
Jiri Mladek <xmladek@fi.muni.cz>
2024-06-23 19:29:52 +0200

    feat: Basic Pending Invites page

--- apps/web/src/hooks/useGroupInvites.ts
+++ apps/web/src/hooks/useGroupInvites.ts
@@ -1,4 +1,4 @@
-import { inviteUserToGroupSchema } from './../../../api/src/api/rest/validationSchemas/user.validationSchemas';
+import { inviteUserToGroupSchema } from "./../../../api/src/api/rest/validationSchemas/user.validationSchemas";
 import usePersistentData from "@/hooks/usePersistentData";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import type { InviteUserToGroupRequest } from "@/types/Api";
@@ -8,14 +8,16 @@ const baseURL = import.meta.env.VITE_API_URL;
 
 const api = new Api({ baseUrl: baseURL });
 
-const QUERY_KEYS = {};
+const QUERY_KEYS = {
+  invites: "invites",
+};
 
 const MUTATION_KEYS = {
   inviteUser: "inviteUser",
 };
 
 // Hook to invite a user to a group
-export const useInviteUserToGroup = (userId : string) => {
+export const useInviteUserToGroup = (userId: string) => {
   const { authData } = usePersistentData();
 
   const mutation = useMutation({
@@ -33,4 +35,21 @@ export const useInviteUserToGroup = (userId : string) => {
   });
 
   return mutation;
+};
+
+// Hook to get all invites for the user using react-query
+export const useInvites = () => {
+  const { authData } = usePersistentData();
+
+  const { data, isLoading } = useQuery({
+    queryKey: [QUERY_KEYS.invites],
+    queryFn: () =>
+      api.user.groupsInvitesList({
+        headers: {
+          Authorization: `Bearer ${authData.token}`, // Add Bearer token to headers
+        },
+      }),
+  });
+
+  return { data, isLoading };
 };
