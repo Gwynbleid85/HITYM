c4a50f9a8708155996f9e88be336a3ea89645604
Gwynbleid85 <hegr.milos85@seznam.cz>
2024-06-25 00:03:36 +0200

    feat: update choose on mail  icon

--- apps/web/src/pages/ChooseOnMap.tsx
+++ apps/web/src/pages/ChooseOnMap.tsx
@@ -1,14 +1,14 @@
 import { ChooseOnMapButtons } from "@/components/ChooseOnMapButtons";
 import Map from "@/components/Map";
 import { PositionConfig } from "@/components/PositionConfig";
-import { CirclePlus } from "lucide-react";
+import { MapPin } from "lucide-react";
 
 function ChooseOnMap() {
   return (
     <Map>
       <PositionConfig />
       <ChooseOnMapButtons />
-      <CirclePlus className="absolute inset-0 m-auto w-10 h-10 z-[400]" />
+      <MapPin className="absolute inset-0 m-auto w-10 h-10 z-[400] -translate-y-4 " />
     </Map>
   );
 }
